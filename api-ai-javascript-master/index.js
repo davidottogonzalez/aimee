38576d19eaae909a705d8824b3867024fe59c6a4
Samuel <sam@bigairsoftware.com>
2016-07-15 14:50:10 -0700

    ai demo

---
+++ api-ai-javascript-master/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(  "/Users/Samuel/Downloads/api-ai-javascript-master/demo/" + "index.html" );
+});
+
+app.use(express.static(path.join(__dirname, '/demo/js')));
+
+app.listen(3000, function () {
+
+  console.log('Example app listening on port 3000!');
+});
