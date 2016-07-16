09231517202bbb24ff80b9542247b34241400977
andimitre <andi.mitre@nbcuni.com>
2016-07-15 22:22:45 -0400

    added bodyparser

--- api-ai-javascript-master/index.js
+++ api-ai-javascript-master/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/demo/index.html" );
